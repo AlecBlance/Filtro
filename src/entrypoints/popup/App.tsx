43bff1c5d69631a62e0aca16a97abffc09831ea7
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2025-08-17 20:44:29 +0800

    refactor: :recycle: restructure App component and add InputTags for better filter management

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,67 +1,6 @@
-import { TagsInput } from "react-tag-input-component";
-import { filter } from "@/utils/storage";
-import { FilterData } from "@/@types";
+import InputTags from "@/entrypoints/popup/components/inputTags";
 
 function App() {
-  const [filterData, setFilterData] = useState<FilterData>({
-    productDescriptionFilter: [],
-    productTitleFilter: [],
-    tagFilter: [],
-    urlFilter: [],
-  });
-  const [isDone, setIsDone] = useState(false);
-  const inputs = [
-    {
-      label: "Title",
-      name: "productTitleFilter",
-    },
-    {
-      label: "Description",
-      name: "productDescriptionFilter",
-    },
-    {
-      label: "Tags",
-      name: "tagFilter",
-    },
-    {
-      label: "URL",
-      name: "urlFilter",
-    },
-  ];
-
-  const setSelected = async ({
-    data,
-    name,
-  }: {
-    data: string[];
-    name: string;
-  }) => {
-    console.log("Setting filter data for:", name, "with data:", data);
-    await filter.setValue({
-      ...filterData,
-      [name]: data,
-    });
-    setFilterData((prev) => ({
-      ...prev,
-      [name]: data,
-    }));
-  };
-
-  useEffect(() => {
-    filter.getValue().then((data) => {
-      setFilterData(data);
-      setIsDone(true);
-    });
-  }, []);
-
-  if (!isDone) {
-    return (
-      <div className="flex items-center justify-center h-full">
-        <p className="text-slate-500">Loading...</p>
-      </div>
-    );
-  }
-
   return (
     <div>
       <div className="flex items-center shadow-[0px_18px_43px_-24px_rgba(0,_0,_0,_0.1)] py-2">
@@ -73,32 +12,15 @@ function App() {
               {import.meta.env.VITE_VERSION}
             </p>
           </div>
-          <p className="text-[10px] text-slate-500">Your Product Hunt Filter</p>
+          <p className="text-slate-500">Your Product Hunt Filter</p>
         </div>
       </div>
       <div className="p-2 space-y-2">
-        <p className="text-[10px] text-slate-500">
+        <p className="text-slate-500">
           Type text to block products if it appears in any section: title,
           description, tags, or URL.
         </p>
-        {inputs.map((input) => {
-          return (
-            <div>
-              <p className="text-slate-700">{input.label}</p>
-              <TagsInput
-                value={filterData[input.name as keyof FilterData]}
-                onChange={(e) => {
-                  setSelected({ data: e, name: input.name });
-                }}
-                name={input.name}
-                placeHolder={`enter ${input.label.toLowerCase()} to block`}
-                classNames={{
-                  input: "text-slate-700",
-                }}
-              />
-            </div>
-          );
-        })}
+        <InputTags />
       </div>
     </div>
   );
---
+++ src/entrypoints/popup/components/inputTags.tsx
@@ -0,0 +1,88 @@
+import { TagsInput } from "react-tag-input-component";
+import { filter } from "@/utils/storage";
+import { FilterData } from "@/@types";
+
+const inputTags = () => {
+  const [filterData, setFilterData] = useState<FilterData>({
+    productDescriptionFilter: [],
+    productTitleFilter: [],
+    tagFilter: [],
+    urlFilter: [],
+  });
+  const [isDone, setIsDone] = useState(false);
+  const inputs = [
+    {
+      label: "Title",
+      name: "productTitleFilter",
+    },
+    {
+      label: "Description",
+      name: "productDescriptionFilter",
+    },
+    {
+      label: "Tags",
+      name: "tagFilter",
+    },
+    {
+      label: "URL",
+      name: "urlFilter",
+    },
+  ];
+
+  const setSelected = async ({
+    data,
+    name,
+  }: {
+    data: string[];
+    name: string;
+  }) => {
+    console.log("Setting filter data for:", name, "with data:", data);
+    await filter.setValue({
+      ...filterData,
+      [name]: data,
+    });
+    setFilterData((prev) => ({
+      ...prev,
+      [name]: data,
+    }));
+  };
+
+  useEffect(() => {
+    filter.getValue().then((data) => {
+      setFilterData(data);
+      setIsDone(true);
+    });
+  }, []);
+
+  if (!isDone) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <p className="text-slate-500">Loading...</p>
+      </div>
+    );
+  }
+  return (
+    <>
+      {inputs.map((input) => {
+        return (
+          <div>
+            <p className="text-slate-700">{input.label}</p>
+            <TagsInput
+              value={filterData[input.name as keyof FilterData]}
+              onChange={(e) => {
+                setSelected({ data: e, name: input.name });
+              }}
+              name={input.name}
+              placeHolder={`enter ${input.label.toLowerCase()} to block`}
+              classNames={{
+                input: "text-slate-700",
+              }}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default inputTags;

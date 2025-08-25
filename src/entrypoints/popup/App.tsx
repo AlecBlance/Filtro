78a3d7f288c4bc6dc80de692248c359b6c6eeae8
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2025-08-25 14:52:04 +0800

    feat: add notice to refresh page for filter application

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -43,6 +43,20 @@ function App() {
           </TabsContent>
         </Tabs>
       </div>
+      <div className="px-4 py-3 flex items-center justify-center bg-primary text-white rounded-t-xl">
+        <b>
+          Please refresh the page to apply filters -{" "}
+          <span className="text-xs mt-1">
+            <a
+              href="https://alecblance.com"
+              target="_blank"
+              className="underline"
+            >
+              Alec Blance
+            </a>
+          </span>
+        </b>
+      </div>
     </div>
   );
 }
